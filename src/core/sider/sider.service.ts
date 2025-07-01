a5cff46a863ea497145ca2c122a8bdb5ef6f2174
Abrorbek <Abrorbek Karimov>
2025-07-01 21:51:20 +0500

    Seeder va model to‘g‘rilandi

--- src/core/sider/sider.service.ts
+++ src/core/sider/sider.service.ts
@@ -5,7 +5,7 @@ import { User } from '../models/user.model/user.model';
 
 @Injectable()
 export class SeedersService implements OnModuleInit {
-  private readonly logger = new Logger("weryil");
+  private readonly logger = new Logger('SeedersService');
 
   constructor(
     @InjectModel(User)
@@ -17,20 +17,26 @@ export class SeedersService implements OnModuleInit {
   }
 
   async userSeeder() {
-    
-    const username =  "AbrorBey"
-    const email = "abrorjonk9@gmail.com"
-    const password = "Abrorbek009"
+    const username = 'AbrorBey';
+    const email = 'abrorjonk9@gmail.com';
+    const password = 'Abrorbek009';
+
+    const existingUser = await this.userModel.findOne({ where: { username } });
+
+    if (existingUser) {
+      this.logger.warn(`❗ User '${username}' already exists. Seeder skipped.`);
+      return;
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await this.userModel.create({
-      username: username,
-      email: email,
-      password: hashedPassword,
-      role: 'SUPERADMIN', 
-      
+      username,
+      email,
+      password_hash: hashedPassword,
+      role: 'SUPERADMIN',
     });
+    
 
     this.logger.log('✅ Superadmin successfully created!');
   }
